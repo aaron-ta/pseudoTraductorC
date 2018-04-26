7c79c81536eaaa2f8cb43fa78fb085c06efb4f1f
Aarón Torres <37317666+aoran-pb@users.noreply.github.com>
2018-04-26 13:03:30 -0700

    Delete programa.c

--- programa.c
+++
@@ -1,7 +0,0 @@
-inicio-programavoid main(){ 
-leer	scanf("Cuantas calificaciones""Cuantas calificaciones"%d,,nn)
-promprom==mientras	while 
-((<<)){leer	scanf("Da una calificacion""Da una calificacion"%d,,calcal)
-promprom==promprom==si	if (((>>))entonces	{ 
-escribir	printf("Aprobado con: ""Aprobado con: "prompromsi	if (((====))entonces	{ 
-escribir	printf("Excelente""Excelente"
